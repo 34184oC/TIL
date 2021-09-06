d6b4329299b266e37f677aa5d099b30ec0112250
mylby <throne81@kakao.com>
2021-09-06 23:01:26 +0900

    Coding Test1-2

--- TEST/codingTest1.js
+++ TEST/codingTest1.js
@@ -1,6 +1,6 @@
 // Zerobace Coding Test1
 
-//1번
+// 1번
 function solve1(nums) {
   let array = new Set();
   const numArray = new Set(nums);
@@ -14,3 +14,31 @@ function solve1(nums) {
  
   return Array.from(answer);
 }
+
+
+// 2번
+let newPuzzle = []; 
+let newWord = "";
+
+function solve2(puzzle, word){
+  newPuzzle = puzzle;
+  newWord = word;
+
+  // newPuzzle에 들어있는 배열 n번 호출
+  for (let i = 0; i < puzzle.length; i++) {
+    // 배열 n번의 0번째 단어 호출
+    for (let j = 0; j < puzzle[0].length; j++) {
+      checkWord(i, j, puzzle.length, puzzle[0].length);
+    }
+  }
+  if(newWord.length == 0 ) return true;
+  return false;
+}
+
+function checkWord( i, j, n, m){
+   // 흑흑 여기서부터 모르겠어요 
+}
+
+
+console.log(solve2([['가', '나', '콜', '사'],['라', '기', '로', '세'],['미',
+'모', '리', '움'],['상', '지', '곤', '타']], '참기름'))
